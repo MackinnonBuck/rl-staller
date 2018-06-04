015034d9cf542684f63b48245a0851da4f3c9fb5
Mackinnon Buck <mackinnonman@icloud.com>
2018-06-03 19:52:42 -0700

    v1.0
    The first version of RLStaller. It currently supports selecting 1 of 4 XBox controllers to read from, and allows the user to select any button on their XBox controller as the "stall" button. The user can also input their air steer left, air roll right, and jump keyboard bindings in Rocket League so RLStaller knows which sequence to perform when the stall button is pressed. The user's preferences are automatically saved so they don't need to be reentered each time RLStaller is launched.

---
+++ RLStaller/MainWindow.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace RLStaller
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.controllerComboBox = new System.Windows.Forms.ComboBox();
+            this.controllerLabel = new System.Windows.Forms.Label();
+            this.stallLabel = new System.Windows.Forms.Label();
+            this.stallComboBox = new System.Windows.Forms.ComboBox();
+            this.notFoundLabel = new System.Windows.Forms.Label();
+            this.airSteerLabel = new System.Windows.Forms.Label();
+            this.airRollLabel = new System.Windows.Forms.Label();
+            this.jumpLabel = new System.Windows.Forms.Label();
+            this.airSteerButton = new System.Windows.Forms.Button();
+            this.airRollButton = new System.Windows.Forms.Button();
+            this.jumpButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // controllerComboBox
+            // 
+            this.controllerComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.controllerComboBox.DisplayMember = "UserIndex";
+            this.controllerComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.controllerComboBox.FormattingEnabled = true;
+            this.controllerComboBox.Location = new System.Drawing.Point(12, 33);
+            this.controllerComboBox.Name = "controllerComboBox";
+            this.controllerComboBox.Size = new System.Drawing.Size(206, 21);
+            this.controllerComboBox.TabIndex = 0;
+            this.controllerComboBox.SelectedIndexChanged += new System.EventHandler(this.ControllerSelectedIndexChanged);
+            // 
+            // controllerLabel
+            // 
+            this.controllerLabel.AutoSize = true;
+            this.controllerLabel.Location = new System.Drawing.Point(13, 13);
+            this.controllerLabel.Name = "controllerLabel";
+            this.controllerLabel.Size = new System.Drawing.Size(65, 13);
+            this.controllerLabel.TabIndex = 1;
+            this.controllerLabel.Text = "Controller ID";
+            // 
+            // stallLabel
+            // 
+            this.stallLabel.AutoSize = true;
+            this.stallLabel.Location = new System.Drawing.Point(12, 57);
+            this.stallLabel.Name = "stallLabel";
+            this.stallLabel.Size = new System.Drawing.Size(61, 13);
+            this.stallLabel.TabIndex = 4;
+            this.stallLabel.Text = "Stall Button";
+            // 
+            // stallComboBox
+            // 
+            this.stallComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.stallComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.stallComboBox.FormattingEnabled = true;
+            this.stallComboBox.Location = new System.Drawing.Point(12, 73);
+            this.stallComboBox.Name = "stallComboBox";
+            this.stallComboBox.Size = new System.Drawing.Size(206, 21);
+            this.stallComboBox.TabIndex = 5;
+            this.stallComboBox.SelectedIndexChanged += new System.EventHandler(this.StallSelectedIndexChanged);
+            // 
+            // notFoundLabel
+            // 
+            this.notFoundLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.notFoundLabel.AutoSize = true;
+            this.notFoundLabel.ForeColor = System.Drawing.Color.Red;
+            this.notFoundLabel.Location = new System.Drawing.Point(119, 13);
+            this.notFoundLabel.Name = "notFoundLabel";
+            this.notFoundLabel.Size = new System.Drawing.Size(99, 13);
+            this.notFoundLabel.TabIndex = 3;
+            this.notFoundLabel.Text = "Controller not found";
+            // 
+            // airSteerLabel
+            // 
+            this.airSteerLabel.AutoSize = true;
+            this.airSteerLabel.Location = new System.Drawing.Point(9, 97);
+            this.airSteerLabel.Name = "airSteerLabel";
+            this.airSteerLabel.Size = new System.Drawing.Size(68, 13);
+            this.airSteerLabel.TabIndex = 6;
+            this.airSteerLabel.Text = "Air Steer Left";
+            // 
+            // airRollLabel
+            // 
+            this.airRollLabel.AutoSize = true;
+            this.airRollLabel.Location = new System.Drawing.Point(9, 139);
+            this.airRollLabel.Name = "airRollLabel";
+            this.airRollLabel.Size = new System.Drawing.Size(68, 13);
+            this.airRollLabel.TabIndex = 7;
+            this.airRollLabel.Text = "Air Roll Right";
+            // 
+            // jumpLabel
+            // 
+            this.jumpLabel.AutoSize = true;
+            this.jumpLabel.Location = new System.Drawing.Point(9, 181);
+            this.jumpLabel.Name = "jumpLabel";
+            this.jumpLabel.Size = new System.Drawing.Size(32, 13);
+            this.jumpLabel.TabIndex = 8;
+            this.jumpLabel.Text = "Jump";
+            // 
+            // airSteerButton
+            // 
+            this.airSteerButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.airSteerButton.Location = new System.Drawing.Point(12, 113);
+            this.airSteerButton.Name = "airSteerButton";
+            this.airSteerButton.Size = new System.Drawing.Size(206, 23);
+            this.airSteerButton.TabIndex = 9;
+            this.airSteerButton.Tag = 0;
+            this.airSteerButton.Text = "None";
+            this.airSteerButton.UseVisualStyleBackColor = true;
+            this.airSteerButton.Click += new System.EventHandler(this.BindButtonClick);
+            this.airSteerButton.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BindButtonKeyDown);
+            this.airSteerButton.Leave += new System.EventHandler(this.BindButtonLeave);
+            // 
+            // airRollButton
+            // 
+            this.airRollButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.airRollButton.Location = new System.Drawing.Point(12, 155);
+            this.airRollButton.Name = "airRollButton";
+            this.airRollButton.Size = new System.Drawing.Size(206, 23);
+            this.airRollButton.TabIndex = 10;
+            this.airRollButton.Tag = 1;
+            this.airRollButton.Text = "None";
+            this.airRollButton.UseVisualStyleBackColor = true;
+            this.airRollButton.Click += new System.EventHandler(this.BindButtonClick);
+            this.airRollButton.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BindButtonKeyDown);
+            this.airRollButton.Leave += new System.EventHandler(this.BindButtonLeave);
+            // 
+            // jumpButton
+            // 
+            this.jumpButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.jumpButton.Location = new System.Drawing.Point(12, 197);
+            this.jumpButton.Name = "jumpButton";
+            this.jumpButton.Size = new System.Drawing.Size(206, 23);
+            this.jumpButton.TabIndex = 11;
+            this.jumpButton.Tag = 2;
+            this.jumpButton.Text = "None";
+            this.jumpButton.UseVisualStyleBackColor = true;
+            this.jumpButton.Click += new System.EventHandler(this.BindButtonClick);
+            this.jumpButton.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BindButtonKeyDown);
+            this.jumpButton.Leave += new System.EventHandler(this.BindButtonLeave);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 231);
+            this.Controls.Add(this.jumpButton);
+            this.Controls.Add(this.airRollButton);
+            this.Controls.Add(this.airSteerButton);
+            this.Controls.Add(this.jumpLabel);
+            this.Controls.Add(this.airRollLabel);
+            this.Controls.Add(this.airSteerLabel);
+            this.Controls.Add(this.stallComboBox);
+            this.Controls.Add(this.stallLabel);
+            this.Controls.Add(this.notFoundLabel);
+            this.Controls.Add(this.controllerLabel);
+            this.Controls.Add(this.controllerComboBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "RLStaller";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CloseWindow);
+            this.Load += new System.EventHandler(this.LoadWindow);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox controllerComboBox;
+        private System.Windows.Forms.Label controllerLabel;
+        private System.Windows.Forms.Label stallLabel;
+        private System.Windows.Forms.ComboBox stallComboBox;
+        private System.Windows.Forms.Label notFoundLabel;
+        private System.Windows.Forms.Label airSteerLabel;
+        private System.Windows.Forms.Label airRollLabel;
+        private System.Windows.Forms.Label jumpLabel;
+        private System.Windows.Forms.Button airSteerButton;
+        private System.Windows.Forms.Button airRollButton;
+        private System.Windows.Forms.Button jumpButton;
+    }
+}
+
---
+++ RLStaller/MainWindow.cs
@@ -0,0 +1,257 @@
+﻿using SharpDX.XInput;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace RLStaller
+{
+    public partial class MainWindow : Form
+    {
+        private const int MaxControllers = 4;
+        private const int NumBindings = 3;
+        private const int UpdateRate = 1;
+
+        private Properties.Settings settings;
+
+        private object updateLock;
+        private bool stallPressed;
+
+        private Controller selectedController;
+        private InputSimulator inputSimulator;
+        private VirtualKeyCode[] keyBindings;
+        private GamepadButtonFlags stallBinding;
+
+        /// <summary>
+        /// Initializes the MainWindow instance.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            settings = Properties.Settings.Default;
+
+            updateLock = new object();
+            stallPressed = false;
+
+            inputSimulator = new InputSimulator();
+            keyBindings = new VirtualKeyCode[NumBindings];
+
+            for (int i = 0; i < MaxControllers; i++)
+            {
+                Controller c = new Controller((UserIndex)i);
+                controllerComboBox.Items.Add(c);
+
+                if (selectedController == null && c.IsConnected)
+                    controllerComboBox.SelectedIndex = i;
+            }
+
+            foreach (var value in Enum.GetValues(typeof(GamepadButtonFlags)))
+                stallComboBox.Items.Add(value);
+        }
+
+        /// <summary>
+        /// Launches the background controller input updater.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LoadWindow(object sender, EventArgs e)
+        {
+            LoadSettings();
+
+            new Thread(new ThreadStart(BackgroundUpdater))
+            {
+                IsBackground = true
+            }.Start();
+        }
+
+        /// <summary>
+        /// Saves the user settings when the window is closed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CloseWindow(object sender, FormClosingEventArgs e)
+        {
+            SaveSettings();
+        }
+
+        /// <summary>
+        /// Loads existing user settings and applies those settings to the
+        /// application.
+        /// </summary>
+        private void LoadSettings()
+        {
+            if (settings.ControllerID >= 0 &&
+                settings.ControllerID < MaxControllers)
+                controllerComboBox.SelectedIndex = settings.ControllerID;
+            else
+                settings.ControllerID = controllerComboBox.SelectedIndex;
+
+            stallComboBox.SelectedIndex = settings.StallButton;
+
+            LoadBindingSettings(settings.AirSteerLeft, airSteerButton);
+            LoadBindingSettings(settings.AirRollRight, airRollButton);
+            LoadBindingSettings(settings.Jump, jumpButton);
+        }
+
+        /// <summary>
+        /// Loads the bindings settings with the given id and value, and
+        /// updates the corresponding button.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        /// <param name="button"></param>
+        private void LoadBindingSettings(int value, Button button)
+        {
+            UpdateBinding((int)button.Tag, (VirtualKeyCode)value);
+            RefreshBindingButtton(button);
+        }
+
+        /// <summary>
+        /// Saves the user settings currently entered into the application.
+        /// </summary>
+        private void SaveSettings()
+        {
+            settings.ControllerID = controllerComboBox.SelectedIndex;
+            settings.StallButton = stallComboBox.SelectedIndex;
+            settings.AirSteerLeft = (int)keyBindings[0];
+            settings.AirRollRight = (int)keyBindings[1];
+            settings.Jump = (int)keyBindings[2];
+
+            settings.Save();
+        }
+
+        /// <summary>
+        /// Continuously updates controller input.
+        /// </summary>
+        private void BackgroundUpdater()
+        {
+            while (true)
+            {
+                lock (updateLock)
+                    UpdateController();
+
+                Thread.Sleep(UpdateRate);
+            }
+        }
+
+        /// <summary>
+        /// Updates controller input for the current selected controller and
+        /// write keyboard output if necessary.
+        /// </summary>
+        private void UpdateController()
+        {
+            if (selectedController == null || !selectedController.IsConnected)
+                return;
+
+            State state;
+
+            try
+            {
+                state = selectedController.GetState();
+            }
+            catch (SharpDX.SharpDXException)
+            {
+                return;
+            }
+
+            if (stallBinding != GamepadButtonFlags.None &&
+                (state.Gamepad.Buttons & stallBinding) != 0)
+            {
+                if (!stallPressed)
+                {
+                    stallPressed = true;
+                    for (int i = 0; i < NumBindings; i++)
+                        inputSimulator.Keyboard.KeyDown(keyBindings[i]);
+                }
+            }
+            else if (stallPressed)
+            {
+                stallPressed = false;
+                for (int i = 0; i < NumBindings; i++)
+                    inputSimulator.Keyboard.KeyUp(keyBindings[i]);
+            }
+        }
+
+        /// <summary>
+        /// Updates the binding for the given binding id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="keyCode"></param>
+        private void UpdateBinding(int id, VirtualKeyCode keyCode)
+        {
+            lock (updateLock)
+                keyBindings[id] = keyCode;
+        }
+
+        /// <summary>
+        /// Updates the text for the given binding button.
+        /// </summary>
+        /// <param name="button"></param>
+        private void RefreshBindingButtton(Button button)
+        {
+            button.Text = ((Keys)keyBindings[(int)button.Tag]).ToString();
+        }
+
+        /// <summary>
+        /// Disables controller input updating, changes the selected
+        /// controller, and reenables controller input updating.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ControllerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            lock (updateLock)
+            {
+                selectedController =
+                    (Controller)controllerComboBox.SelectedItem;
+                notFoundLabel.Visible = !selectedController.IsConnected;
+            }
+        }
+
+        /// <summary>
+        /// Disables controller input updating, updates the stall binding,
+        /// and reenables controller input updating.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StallSelectedIndexChanged(object sender, EventArgs e)
+        {
+            lock (updateLock)
+                stallBinding = (GamepadButtonFlags)stallComboBox.SelectedItem;
+        }
+
+        /// <summary>
+        /// Updates the bind button text when a bind button is clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BindButtonClick(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = "<press any key>";
+        }
+
+        /// <summary>
+        /// Remaps the keybinding corresponding selected button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BindButtonKeyDown(object sender, KeyEventArgs e)
+        {
+            UpdateBinding((int)(((Button)sender).Tag), (VirtualKeyCode)e.KeyCode);
+
+            ActiveControl = controllerLabel;
+        }
+
+        /// <summary>
+        /// Sets the text of the given button when focus is left
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BindButtonLeave(object sender, EventArgs e)
+        {
+            RefreshBindingButtton((Button)sender);
+        }
+    }
+}
